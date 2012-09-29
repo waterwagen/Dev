48a629f75fed73bffc3ad2ec1e0ed1da3f924431
Josh Taylor <waterwagen@gmail.com>
2012-09-29 22:11:07 +0000

    Another iteration for getting acceptance tests through Fitnesse working in the build server. This commit checks in the whole FitNesse root directory and adds an optional task to the gradle build file which starts FitNesse and opens the home page in the browser to facilitate editing (or manually running) acceptance tests.

---
+++ src/test/java/com/waterwagen/mris/FirstTestFitnesseFixture.java
@@ -0,0 +1,22 @@
+package com.waterwagen.mris;
+
+public class FirstTestFitnesseFixture
+{
+	private int mInput1;
+	private int mInput2;
+	
+	public int output1() 
+	{
+		return mInput1 + mInput2;
+	}
+	
+	public void setInput1(int input1)
+	{
+		mInput1 = input1;
+	}
+	
+	public void setInput2(int input2)
+	{
+		mInput2 = input2;
+	}
+}
