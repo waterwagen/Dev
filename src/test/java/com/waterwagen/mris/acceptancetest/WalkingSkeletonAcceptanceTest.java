2f7f71b6a66d245491f0d217b6b09c11d3f121e3
Josh Taylor <waterwagen@gmail.com>
2012-10-10 19:50:50 +0000

    Added some ideas for the beginnings of the walking skeleton acceptance test code.

--- src/test/java/com/waterwagen/mris/acceptancetest/WalkingSkeletonAcceptanceTest.java
+++ src/test/java/com/waterwagen/mris/acceptancetest/WalkingSkeletonAcceptanceTest.java
@@ -6,10 +6,13 @@ import org.junit.Test;
 
 public class WalkingSkeletonAcceptanceTest
 {
-
 	@Test
 	public void test()
 	{
+//		ApplicationRunner application = ApplicationRunner.buildApplicationRunner();
+//		application.display();
+//		application.inputContactInfoFile();
+//		application.hasNewContactInfoFileWithEmailAddressesInApplicationRootDirectory();
 		System.out.println("Running acceptance test.");
 //		fail("Ran acceptance test one!");
 	}
