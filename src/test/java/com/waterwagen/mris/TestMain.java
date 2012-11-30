48584e61895c53e5877922fe7e8c9b9f6a5de1a3
Josh Taylor <waterwagen@gmail.com>
2012-11-30 19:30:09 +0000

    Update to run the test with the GUI window not visible by using the Cacio-TTA JUnit runner.

--- src/test/java/com/waterwagen/mris/TestMain.java
+++ src/test/java/com/waterwagen/mris/TestMain.java
@@ -2,9 +2,13 @@ package com.waterwagen.mris;
 
 import java.lang.reflect.InvocationTargetException;
 
+import net.java.openjdk.cacio.ctc.junit.CacioTestRunner;
+
 import org.fest.util.Arrays;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(CacioTestRunner.class)
 public class TestMain extends SwingTestCase
 {
 	@Test
